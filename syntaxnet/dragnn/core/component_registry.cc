ef203fd68501d23c4b9468521a76df0eb9ef9a5f
mhsamavatian <mh.samavatian@gmail.com>
2018-11-12 12:23:04 -0500

    delete

--- syntaxnet/dragnn/core/component_registry.cc
+++
@@ -1,8 +0,0 @@
-#include "dragnn/core/component_registry.h"
-
-namespace syntaxnet {
-
-// Class registry for DRAGNN components.
-REGISTER_SYNTAXNET_CLASS_REGISTRY("DRAGNN Component", dragnn::Component);
-
-}  // namespace syntaxnet
--- syntaxnet/dragnn/python/dragnn_ops.py
+++
@@ -1,9 +0,0 @@
-"""Groups the DRAGNN TensorFlow ops in one module."""
-
-
-try:
-  from dragnn.core.ops.gen_dragnn_bulk_ops import *
-  from dragnn.core.ops.gen_dragnn_ops import *
-except ImportError as e:
-    raise e
-
